03472d1ead1d556de886d0e31e560f8763367ed2
Tyler James Newman <tyler.newman@runtriz.com>
2016-08-16 02:05:19 -0700

    style instagram page

--- config/environment.js
+++ config/environment.js
@@ -24,7 +24,7 @@ module.exports = function (environment) {
     'script-src': "'self' 'unsafe-inline'",
     'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
     'font-src': "'self' fonts.gstatic.com",
-    'connect-src': "'self'",
+    'connect-src': "'self' http://localhost:*/ https://www.instagram.com/*",
     'img-src': "'self' data:",
     'media-src': "'self'",
   };
